49872dd2bb7181ae7ab090de2ac1da4a6737781b
aaronPeter <aaronpeter@pop-os.localdomain>
2023-04-14 09:25:49 +0300

    adding controls to the player

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,33 @@ import ReactPlayer from "react-player";
 import screenfull from "screenfull";
 import { Container } from "@mui/material";
 import ControlIcons from "./Components/ControlIcons";
+import { useState, useRef } from "react";
 
 function App() {
   // return <div className="App">
 
   //   {/* <Player /> */}</div>;
+  const [playerstate, setPlayerState] = useState({
+    playing: true,
+    mute: true,
+    volume: 0.5,
+    playerbackRate: 1.0,
+    played: 0,
+    seeking: false,
+  });
+
+  const { playing, mute, volume, playerbackRate, played, seeking } =
+    playerstate;
+
+  const handlePlayAndPause = () => {
+    setPlayerState({
+      ...playerstate,
+      playing: !playerstate.playing,
+    });
+  };
+
+  const playerRef = useRef(null);
+
   return (
     <>
       <header className="header__section">
@@ -20,10 +42,11 @@ function App() {
             width={"100%"}
             height="100%"
             url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
-            playing={false}
-            muted={true}
+            ref={playerRef}
+            playing={playing}
+            muted={mute}
           />
-          <ControlIcons />
+          <ControlIcons playandpause={handlePlayAndPause} playing={playing} />
         </div>
       </Container>
     </>
