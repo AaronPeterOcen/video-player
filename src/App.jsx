c38ab15ea1a890b8205d598f3566a0f28f57828c
aaronPeter <aaronpeter@pop-os.localdomain>
2023-04-17 09:57:08 +0300

    finished adding the functionality of the player

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,158 @@
 import * as React from "react";
-import "./App.css";
+import { useState, useRef } from "react";
 import ReactPlayer from "react-player";
 import screenfull from "screenfull";
-import { Container } from "@mui/material";
+import Container from "@mui/material/Container";
+import "./App.css";
 import ControlIcons from "./Components/ControlIcons";
-import { useState, useRef } from "react";
+
+const format = (seconds) => {
+  if (isNaN(seconds)) {
+    return "00:00";
+  }
+
+  const date = new Date(seconds * 1000);
+  const hh = date.getUTCHours();
+  const mm = date.getUTCMinutes();
+  const ss = date.getUTCSeconds().toString().padStart(2, "0");
+
+  if (hh) {
+    return `${hh}:${mm.toString().padStart(2, "0")}:${ss}`;
+  } else {
+    return `${mm}:${ss}`;
+  }
+};
 
 function App() {
-  // return <div className="App">
-
-  //   {/* <Player /> */}</div>;
   const [playerstate, setPlayerState] = useState({
     playing: true,
-    mute: true,
+    muted: true,
     volume: 0.5,
     playerbackRate: 1.0,
     played: 0,
     seeking: false,
   });
 
-  const { playing, mute, volume, playerbackRate, played, seeking } =
+  //Destructure State in other to get the values in it
+  const { playing, muted, volume, playerbackRate, played, seeking } =
     playerstate;
+  const playerRef = useRef(null);
+  const playerDivRef = useRef(null);
 
+  //This function handles play and pause onchange button
   const handlePlayAndPause = () => {
+    setPlayerState({ ...playerstate, playing: !playerstate.playing });
+  };
+
+  const handleMuting = () => {
+    setPlayerState({ ...playerstate, muted: !playerstate.muted });
+  };
+
+  const handleRewind = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() - 10);
+  };
+
+  const handleFastForward = () => {
+    playerRef.current.seekTo(playerRef.current.getCurrentTime() + 30);
+  };
+
+  const handleVolumeChange = (e, newValue) => {
     setPlayerState({
       ...playerstate,
-      playing: !playerstate.playing,
+      volume: parseFloat(newValue / 100),
+      muted: newValue === 0 ? true : false,
     });
   };
 
-  const playerRef = useRef(null);
+  const handleVolumeSeek = (e, newValue) => {
+    setPlayerState({
+      ...playerstate,
+      volume: parseFloat(newValue / 100),
+      muted: newValue === 0 ? true : false,
+    });
+  };
+
+  const handlePlayerRate = (rate) => {
+    setPlayerState({ ...playerstate, playerbackRate: rate });
+  };
+
+  const handleFullScreenMode = () => {
+    screenfull.toggle(playerDivRef.current);
+  };
+
+  const handlePlayerProgress = (state) => {
+    console.log("onProgress", state);
+    if (!playerstate.seeking) {
+      setPlayerState({ ...playerstate, ...state });
+    }
+    console.log("afterProgress", state);
+  };
+
+  const handlePlayerSeek = (e, newValue) => {
+    setPlayerState({ ...playerstate, played: parseFloat(newValue / 100) });
+    playerRef.current.seekTo(parseFloat(newValue / 100));
+    // console.log(played)
+  };
+
+  const handlePlayerMouseSeekDown = (e) => {
+    setPlayerState({ ...playerstate, seeking: true });
+  };
+
+  const handlePlayerMouseSeekUp = (e, newValue) => {
+    setPlayerState({ ...playerstate, seeking: false });
+    playerRef.current.seekTo(newValue / 100);
+  };
+
+  const currentPlayerTime = playerRef.current
+    ? playerRef.current.getCurrentTime()
+    : "00:00";
+  const movieDuration = playerRef.current
+    ? playerRef.current.getDuration()
+    : "00:00";
+  const playedTime = format(currentPlayerTime);
+  const fullMovieTime = format(movieDuration);
 
   return (
     <>
       <header className="header__section">
-        <p className="header__text">Build a video Player Tut</p>
+        <p className="header__text">Build a Video Player - Tutorial</p>
       </header>
       <Container maxWidth="md">
-        <div className="playerDiv">
+        <div className="playerDiv" ref={playerDivRef}>
           <ReactPlayer
             width={"100%"}
             height="100%"
-            url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
             ref={playerRef}
+            url="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/TearsOfSteel.mp4"
             playing={playing}
-            muted={mute}
+            volume={volume}
+            playbackRate={playerbackRate}
+            onProgress={handlePlayerProgress}
+            muted={muted}
           />
-          <ControlIcons playandpause={handlePlayAndPause} playing={playing} />
+
+          <ControlIcons
+            key={volume.toString()}
+            playandpause={handlePlayAndPause}
+            playing={playing}
+            rewind={handleRewind}
+            fastForward={handleFastForward}
+            muting={handleMuting}
+            muted={muted}
+            volumeChange={handleVolumeChange}
+            volumeSeek={handleVolumeSeek}
+            volume={volume}
+            playerbackRate={playerbackRate}
+            playRate={handlePlayerRate}
+            fullScreenMode={handleFullScreenMode}
+            played={played}
+            onSeek={handlePlayerSeek}
+            onSeekMouseUp={handlePlayerMouseSeekUp}
+            onSeekMouseDown={handlePlayerMouseSeekDown}
+            playedTime={playedTime}
+            fullMovieTime={fullMovieTime}
+            seeking={seeking}
+          />
         </div>
       </Container>
     </>
--- src/Components/ControlIcons.jsx
+++ src/Components/ControlIcons.jsx
@@ -2,20 +2,62 @@ import React from "react";
 import "./ControlIcons.css";
 import Grid from "@mui/material/Grid";
 import Typography from "@mui/material/Typography";
-
-import { PauseSharp } from "@mui/icons-material";
-import { IconButton } from "@mui/material";
-import { FastForwardSharp } from "@mui/icons-material";
-import { FastRewind } from "@mui/icons-material";
-import { PlayArrowSharp } from "@mui/icons-material";
-
 import Button from "@mui/material/Button";
 import Slider from "@mui/material/Slider";
 import { styled } from "@mui/material/styles";
+import { IconButton } from "@mui/material";
+import { FastRewind } from "@mui/icons-material";
+import { FastForwardSharp } from "@mui/icons-material";
+import { PlayArrowSharp } from "@mui/icons-material";
+import { PauseSharp } from "@mui/icons-material";
 import { VolumeUp } from "@mui/icons-material";
+import { VolumeOff } from "@mui/icons-material";
 import { Fullscreen } from "@mui/icons-material";
+import Popover from "@mui/material/Popover";
+import Tooltip from "@mui/material/Tooltip";
 
-const ControlIcons = ({ playandpause, playing }) => {
+const ControlIcons = ({
+  playandpause,
+  playing,
+  rewind,
+  fastForward,
+  muting,
+  muted,
+  volumeChange,
+  volumeSeek,
+  volume,
+  playRate,
+  playerbackRate,
+  fullScreenMode,
+  onSeek,
+  played,
+  onSeekMouseUp,
+  onSeekMouseDown,
+  fullMovieTime,
+  playedTime,
+}) => {
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const handlePopOver = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? "playbackrate-popover" : undefined;
+
+  function ValueLabelComponent(props) {
+    const { children, value } = props;
+
+    return (
+      <Tooltip enterTouchDelay={0} placement="top" title={value}>
+        {children}
+      </Tooltip>
+    );
+  }
+
   const PrettoSlider = styled(Slider)({
     height: 5,
     "& .MuiSlider-track": {
@@ -53,10 +95,9 @@ const ControlIcons = ({ playandpause, playing }) => {
       },
     },
   });
-
   return (
     <div className="controls__div">
-      {/* Top Segment */}
+      {/* Top Controls */}
       <Grid
         container
         direction="row"
@@ -71,14 +112,18 @@ const ControlIcons = ({ playandpause, playing }) => {
         </Grid>
       </Grid>
 
-      {/* Middle Segment */}
+      {/* Middle Controls */}
       <Grid
         container
         direction="row"
         alignItems="center"
         justifyContent="center"
       >
-        <IconButton className="controls__icons" aria-label="reqind">
+        <IconButton
+          className="controls__icons"
+          aria-label="reqind"
+          onClick={rewind}
+        >
           <FastRewind fontSize="large" style={{ color: "white" }} />
         </IconButton>
 
@@ -94,12 +139,16 @@ const ControlIcons = ({ playandpause, playing }) => {
           )}
         </IconButton>
 
-        <IconButton className="controls__icons" aria-label="reqind">
+        <IconButton
+          className="controls__icons"
+          aria-label="reqind"
+          onClick={fastForward}
+        >
           <FastForwardSharp fontSize="large" style={{ color: "white" }} />
         </IconButton>
       </Grid>
 
-      {/* Bottom Segment */}
+      {/* Bottom Controls */}
       <Grid
         container
         direction="row"
@@ -114,13 +163,25 @@ const ControlIcons = ({ playandpause, playing }) => {
         </Grid>
 
         <Grid item xs={12}>
-          <PrettoSlider min={0} max={100} defaultValue={20} />
+          <PrettoSlider
+            min={0}
+            max={100}
+            value={played * 100}
+            onChange={onSeek}
+            onMouseDown={onSeekMouseDown}
+            onChangeCommitted={onSeekMouseUp}
+            valueLabelDisplay="auto"
+            // aria-label="custom thumb label"
+            components={{
+              ValueLabel: ValueLabelComponent,
+            }}
+          />
           <Grid container direction="row" justifyContent="space-between">
             <Typography variant="h7" style={{ color: "white" }}>
-              00:26
+              {playedTime}
             </Typography>
             <Typography variant="h7" style={{ color: "white" }}>
-              12:30
+              {fullMovieTime}
             </Typography>
           </Grid>
         </Grid>
@@ -139,28 +200,69 @@ const ControlIcons = ({ playandpause, playing }) => {
               )}
             </IconButton>
 
-            <IconButton className="controls__icons" aria-label="reqind">
-              <VolumeUp fontSize="large" style={{ color: "white" }} />
+            <IconButton
+              className="controls__icons"
+              aria-label="reqind"
+              onClick={muting}
+            >
+              {muted ? (
+                <VolumeOff fontSize="large" style={{ color: "white" }} />
+              ) : (
+                <VolumeUp fontSize="large" style={{ color: "white" }} />
+              )}
             </IconButton>
 
             <Typography style={{ color: "#fff", paddingTop: "5px" }}>
-              40
+              {volume * 100}
             </Typography>
             <Slider
               min={0}
               max={100}
-              defaultValue={100}
+              value={volume * 100}
+              onChange={volumeChange}
+              onChangeCommitted={volumeSeek}
               className="volume__slider"
             />
           </Grid>
         </Grid>
 
         <Grid item>
-          <Button variant="text" className="bottom__icons">
-            <Typography>1X</Typography>
+          <Button
+            variant="text"
+            onClick={handlePopOver}
+            className="bottom__icons"
+          >
+            <Typography>{playerbackRate}X</Typography>
           </Button>
 
-          <IconButton className="bottom__icons">
+          <Popover
+            id={id}
+            open={open}
+            anchorEl={anchorEl}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "center",
+            }}
+            transformOrigin={{
+              vertical: "bottom",
+              horizontal: "center",
+            }}
+          >
+            <Grid container direction="column-reverse">
+              {[0.5, 1, 1.5, 2].map((rate) => (
+                <Button variant="text" onClick={() => playRate(rate)}>
+                  <Typography
+                    color={rate === playerbackRate ? "secondary" : "default"}
+                  >
+                    {rate}
+                  </Typography>
+                </Button>
+              ))}
+            </Grid>
+          </Popover>
+
+          <IconButton className="bottom__icons" onClick={fullScreenMode}>
             <Fullscreen fontSize="large" />
           </IconButton>
         </Grid>
